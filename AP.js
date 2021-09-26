56e165a57bab0a0b046cb472cb978c4d32a48386
coffee1118 <91401529+coffee1118@users.noreply.github.com>
2021-09-26 13:48:39 +0900

    Update AP.js

--- AP.js
+++ AP.js
@@ -51,17 +51,7 @@ javascript: (
     function outputTsv() {
       let win = window.open('', 'name', 'height=250,width=700');
       win.document.write('<html><head><title>Amazon to TSV</title>');
-      win.document.write(`
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-XXXXXX');</script>
-<!-- End Google Tag Manager -->
-</head>
-<body>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NBHQLZ3" height="10" width="10" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-`);
+
       win.document.write('<pre>');
       win.document.write('注文番号\t注文日\t金額\t商品名\tURL\n');
       win.document.write(content);
